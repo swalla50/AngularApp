35e932e2e30c7215c3610dccdc56b7004b0b9146
swalla50 <stevenwallacejr@aol.com>
2021-10-30 18:07:13 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+  <app-navbar></app-navbar>
+  <app-home></app-home>
+  `
 })
 export class AppComponent {
   title = 'angular-app';
